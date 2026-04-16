29c1286733c3bdb148eaab90acef2402f9342113
Kaleb Garrett <kalebagarrett@gmail.com>
2026-04-16 12:49:24 -0400

    Create profile model

---
+++ ClickForge.Models/Profile.cs
@@ -0,0 +1,9 @@
+﻿namespace ClickForge.Models;
+
+public class Profile
+{
+    public string Name { get; set; }
+    public bool Loop { get; set; }
+    public string WindowName { get; set; }
+    public List<Action> Actions { get; set; }
+}
