1990a8a2a3e5bbbb240961a7901d635d6952cc59
Kaleb Garrett <kalebagarrett@gmail.com>
2026-05-02 13:41:42 -0400

    Add profile repo

---
+++ ClickForge.Profiles/ProfileRepository.cs
@@ -0,0 +1,56 @@
+﻿using ClickForge.Models;
+using System.Text.Json;
+
+namespace ClickForge.Profiles;
+
+public class ProfileRepository
+{
+    public void Create(Profile profile)
+    {
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true
+        };
+
+        var json = JsonSerializer.Serialize(profile, options);
+        File.WriteAllText($@"{Directories.ProfileDirectory}\{profile.Name}.json", json);
+    }
+
+    public void Load(string name)
+    {
+        var json = File.ReadAllText($@"{Directories.ProfileDirectory}\{name}.json");
+
+        var profile = JsonSerializer.Deserialize<Profile>(json);
+        
+        // Move to Program class. Return profile?
+        Console.WriteLine($"\nProfile Name: {profile!.Name}");
+        Console.WriteLine($"Is Loopable: {profile.Loop}");
+        Console.WriteLine($"Target Window Name: {profile.WindowName}");
+        Console.WriteLine($"Input Type: {profile.Action.InputType}");
+        Console.WriteLine($"Input Data: {profile.Action.InputData}");
+        Console.WriteLine($"Delay: {profile.Action.Delay}\n");
+    }
+
+    public void Delete(string name) =>
+        File.Delete($@"{Directories.ProfileDirectory}\{name}.json");
+
+
+    public string?[] GetFileNames() =>
+        GetFiles().Select(Path.GetFileNameWithoutExtension).ToArray();
+
+
+    private string[] GetFiles()
+    {
+        if (!Directory.Exists(Directories.ProfileDirectory))
+        {
+            Directory.CreateDirectory(Directories.ProfileDirectory);
+        }
+
+        return Directory.GetFiles
+        (
+            Directories.ProfileDirectory,
+            "*.json",
+            SearchOption.AllDirectories
+        );
+    }
+}
