81e8be7923d7eab3dcef55943d9673717c4c552d
Kaleb Garrett <kalebagarrett@gmail.com>
2026-05-02 13:41:06 -0400

    Add directories

---
+++ ClickForge.Profiles/Directories.cs
@@ -0,0 +1,7 @@
+﻿namespace ClickForge.Profiles;
+
+public static class Directories
+{
+    private static readonly string Username = Environment.UserName;
+    public static readonly string ProfileDirectory = $@"C:\Users\{Username}\AppData\Roaming\ClickForge\Profiles";
+}
